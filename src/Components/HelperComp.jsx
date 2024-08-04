2e946219ca006219cce0a45dbb62ad2d0abb559b
Kris Wong <krischwong@gmail.com>
2024-08-04 18:42:54 -0400

    css for person inputs

--- src/Components/HelperComp.jsx
+++ src/Components/HelperComp.jsx
@@ -2,9 +2,10 @@
 
 function Input({ label, name, value, onChange }) {
   return (
-    <label>
-      {label} <input name={name} value={value} onChange={onChange}></input>
-    </label>
+    <div>
+      <label>{label}</label>
+      <input name={name} value={value} onChange={onChange}></input>
+    </div>
   );
 }
 
